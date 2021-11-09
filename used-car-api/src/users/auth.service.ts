6e98efd99628997d14b8ddbab22e44c67657c1cc
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2021-11-09 17:41:40 +0900

    bcrypt, sign up, sign in

--- used-car-api/src/users/auth.service.ts
+++ used-car-api/src/users/auth.service.ts
@@ -1,7 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import {
+    BadRequestException,
+    Injectable,
+    NotFoundException
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
     constructor(private usersService: UsersService) {}
+    async signup(email: string, password: string) {
+        // Validate Email
+        const users = await this.usersService.find(email);
+        if (users) throw new BadRequestException('email in use');
+
+        // Hash password
+        // // SYNC
+        // const salt = bcrypt.genSaltSync(16);
+        // console.log(salt);
+        // const hash = bcrypt.hashSync(password, salt)
+        // ASYNC
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+        // Create new user and save
+        const user = await this.usersService.create(email, hash);
+        // Return user
+        return user;
+    }
+
+    async signin(email: string, password: string) {
+        const [user] = await this.usersService.find(email);
+        if (!user) throw new NotFoundException('user not found');
+        if (!(await bcrypt.compare(password, user.password)))
+            throw new BadRequestException('bad password');
+        return user;
+    }
 }
